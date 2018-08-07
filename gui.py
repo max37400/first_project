57ca790d4024adc3a3b11f4c2a988a103a5430db
max37400 <33259422+max37400@users.noreply.github.com>
2018-08-07 16:04:56 +0300

    Update gui.py
    Добавил создание окна по кнопке распределить (временное решение, будет изменено в дальнейших билдах)

--- gui.py
+++ gui.py
@@ -1,11 +1,17 @@
+#графическое окно для ввода инофрмации и файла с данными
 import tkinter
 root = tkinter.Tk()
+
+def create_new_window(title='Статус распределения'):
+    new_window = tkinter.Tk()
+    new_window.title(title)
+    new_window.mainloop()
 
 w, h = root.winfo_screenwidth(), root.winfo_screenheight()
 root.winfo_toplevel().title("Распределитель онлайн")
 root.geometry('{}x{}+5+5'.format(int(w/3),int(h/3)))
 root.resizable(width=False, height=False)
-b=tkinter.Button(root,activebackground = "red",text="Распределить!")
+b=tkinter.Button(root,activebackground = "red",text="Распределить!",command = create_new_window)
 close = tkinter.Button(root,activebackground='green',text='Закрыть',command = root.destroy)
 b.place(x=w/3-100,y=h/3-80)
 close.place(x=w/3-100,y=h/3-40)
